9004729b85cacc5a78858268db87cf54fbd548ee
Adolfo Hoyos Díaz <adolfo.hoyos@javerianacali.edu.co>
2017-05-04 15:36:09 -0500

    script finished! Report pending

--- module2/05final/code/main2.R
+++ module2/05final/code/main2.R
@@ -1,6 +1,10 @@
 setwd("~/code/doc/bioinformaticscourse/module2/05final/code")
 rm(list = ls())
 source("~/code/fito_lib/r/pkgtest.R")
+my.hclust <- function(x, method = "complete", dmeth = "euclidean"){
+  hclust(dist(x, method = dmeth), method = method)
+}
+
 #### dependencies
 
 pkgTest('dendextend')
@@ -17,6 +21,7 @@ avillin.join <- as.data.frame(read.table("../data/avillin-join-lnx.values"))
 # 1
 
 N = 1000
+
 datasets <- split(
               avillin.join, 
               cut(
@@ -70,5 +75,61 @@ medoids.dataset <- as.data.frame(medoids[[2]])
 
 summary(avillin.join)
 summary(medoids.dataset)
+
+# 4
+
+k <- 4
+
+## Hierarchical clustering
+hierarch.clusters             <- my.hclust(medoids.dataset)
+
+hierarch.cluster.dendrogram   <- as.dendrogram(hierarch.clusters)
+hierarch.cluster.dendrogram   <- color_branches(hierarch.cluster.dendrogram, k = k)
+
+hierarch.clusters.cut         <- cutree(hierarch.cluster.dendrogram, k)
+
+hierarch.clusters.table       <- table(hierarch.clusters.cut)
+
+## K-means
+kmeans.clusters               <- kmeans(medoids.dataset, centers = 4, nstart = 30)
+kmeans.clusters$cluster       <- as.factor(kmeans.clusters$cluster)
+
+kmeans.clusters.table         <- table(kmeans.clusters$cluster)
+
+
+## Plots
+
+### Hierarchichal
+
+plot.colors <- rainbow_hcl(k)
+jpeg('../images/02/hierarch.dendrogram.jpg')
+plot(hierarch.cluster.dendrogram, main = "Hierarchical clustering")
+dev.off()
+
+jpeg('../images/02/hierarch.histogram.jpg')
+bp <- barplot(hierarch.clusters.table, col = plot.colors,
+              xlab = "clusters", ylab = "number of elements", 
+              main = "Hierarchical clustering histogram")
+text(bp, 0, hierarch.clusters.table, cex = 1, pos = 3)
+dev.off()
+
+jpeg('../images/02/hierarchical.clustering.jpg')
+ggplot(medoids.dataset, aes(PC1, PC2, color = as.factor(hierarch.clusters.cut))
+) + geom_point() + ggtitle("Hierarchical clustering")
+dev.off()
+
+### k-means
+
+jpeg('../images/02/kmeans.histogram.jpg')
+bp <- barplot(kmeans.clusters.table, col = plot.colors, xlab = "clusters", ylab = "number of elements", 
+              main = "k-means histrogram")
+text(bp, 0, kmeans.clusters.table, cex = 1, pos = 3)
+dev.off()
+
+jpeg('../images/02/kmeans.clustering.jpg')
+ggplot(medoids.dataset, aes(PC1, PC2, color = kmeans.clusters$cluster)
+) + geom_point() + ggtitle("k-means clustering")
+dev.off()
+
 
 
