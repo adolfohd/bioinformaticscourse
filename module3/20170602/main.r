18016ce2981b0b258e96860ef1ef84f379444c7e
Adolfo Hoyos <adolfo.hoyos@correounivalle.edu.co>
2017-07-08 11:54:08 -0500

    fixed clustering plot

--- module3/20170602/main.r
+++ module3/20170602/main.r
@@ -14,6 +14,7 @@ n.interactions.dataset$log.length           <- as.numeric(log(as.numeric(n.inter
 n.interactions.dataset$log.bitscore         <- as.numeric(log(as.numeric(n.interactions.dataset$mean.bitscore)))
 
 # All
+
 source("plot.interactions.histogram.r")
 jpeg("images/histograms/interaction.histogram.jpg")
 plot.interactions.histogram(n.interactions.dataset, 25)
@@ -40,18 +41,19 @@ wss <- array(NA, 15)
 for (i in 2:15)
     wss[i] <- sum(kmeans(n.interactions.dataset, centers = i)$withinss)
 jpeg("images/sumofsquaresVSnumberofgroups.jpg")
-  plot(1:15, wss, type ="b", xlab = "Number of cluster", ylab = "withinss groups sum of error squares")
+  plot(1:15, wss, type ="b", xlab = "Number of clusters", ylab = "withinss groups sum of error squares")
 dev.off()
 
 library(vegan)
-fit <- cascadeKM(scale(n.interactions.dataset, center =  TRUE, scale = TRUE), 1, 10, iter = 1000)
+fit <- cascadeKM(scale(d, center =  TRUE, scale = TRUE), 1, 10, iter = 1000)
 jpeg("images/kalinskicriterion.jpg")
   plot(fit, sortg = TRUE, grpmts.plot = TRUE)
 dev.off()
 
 
-k <- 6 # Best K accorrding to both methods
+k <-6 # Best K accorrding to both methods
 d <- as.data.frame(d)
+# d <- n.interactions.dataset[,2:8]
 kmeans.clustering <- kmeans(d, centers = k)
 
 # n.interactions.dataset <- n.interactions.dataset[order(kmeans.clustering$cluster, decreasing = T),]
@@ -68,7 +70,7 @@ plot(n.interactions.dataset[,c(1,4)], col= rainbow_hcl(k)[kmeans.clustering$clus
      ylab = ("Average Length")
 )
 legend("topright", pch=16, col=rainbow_hcl(k),
-       legend=unique(kmeans.clustering$cluster))
+       legend=1:k)
 dev.off()
 
 # points(kmeans.clustering$centers, col=1:5,pch=8,cex=1)
@@ -95,17 +97,52 @@ for (i in 1:k){
 }
 
 best.cluster <- readline("Please analize the plots in images/clusters/, and input the label of one chosen cluster : \n") 
-
 filtered.dataset <- n.interactions.dataset[kmeans.clustering$cluster==best.cluster,]
-
 write.csv(filtered.dataset, "filtered.dataset.csv")
 
-# Now we need to look for these IDs in the next source file
-
+'
+# Now we need to look for these IDs in the next source file ???
 source.cytos.dataset <- read.csv("../20170525/reference/blastresumen.cytos.txt", sep = "\t")
-
-
 grepl(source.cytos.dataset$Nodo1[1], rownames(filtered.dataset))
+'
+###
+
+filtered.dataset <- read.csv("filtered.dataset.csv")
+
+###
+library(vegan)
+fit <- cascadeKM(scale(filtered.dataset[,-1], center =  TRUE, scale = TRUE), 1, 10, iter = 1000)
+jpeg("images/kalinskicriterion_filtered.jpg")
+plot(fit, sortg = TRUE, grpmts.plot = TRUE)
+dev.off()
+
+k.f <- 20 # Best K accorrding to both methods
+d.f <- as.data.frame(filtered.dataset)
+kmeans.clustering.filtered <- kmeans(d.f[,2:8], centers = k.f)
+
+n.items <- array(NA, k.f)
+for (i in 1:k.f){
+  library(colorspace)
+  n.items[i] <- sum(kmeans.clustering.filtered$cluster==i)
+  jpeg(paste("images/clusters/filtered/cluster", i, "_", n.items[i] , "items.jpg"))
+  plot(filtered.dataset[, c(2,5)], 
+       col = rainbow_hcl(2)[((kmeans.clustering.filtered$cluster == i) + 1)], 
+       log = "xy",
+       main = (paste("Cluster", i, ": ", n.items[i] , " items")),
+       xlab = ("Number of interactions"),
+       ylab = ("Average Length")
+  )
+  dev.off()
+}
+
+
+
+## knn
+
+library(class)
+k.f <- 20
+
+knn.filtered <- knn()
 
 
 
